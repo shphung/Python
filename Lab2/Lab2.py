86056e26f646d9931dafe3aad14cb145ea7d8b28
Steven <stevenphunghuynh@yahoo.com>
2020-07-02 20:06:43 -0700

    Create Lab2.py

---
+++ Lab2/Lab2.py
@@ -0,0 +1,52 @@
+#Author: Steven Phung
+#Assignment: Lab #2
+#Completed (or Last Revision): 1/29/20
+
+#Task 1
+#1.
+#age1 = int(input("Please enter age 1: "))
+#age2 = int(input("Please enter age 2: "))
+#age3 = int(input("Please enter age 3: "))
+#age4 = int(input("Please enter age 4: "))
+#age5 = int(input("Please enter age 5: "))
+#totalAge = age1 + age2 + age3 + age4 + age5
+#print("Average age using integer division: ", totalAge // 5)
+#print("Average age using float division: ", totalAge / 5)
+
+#2.
+#print(2 ** 120)
+
+#3.
+#print(15 * (1/3))
+
+#4.
+#a) _abc123 b) _123 c) abc123_
+
+#Task 2
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+dreamJob = input("Enter the company you wish to work for: ")
+dreamSalary = int(input("Enter monthly salary you wish to earn in dollars: "))
+
+print("\nMy name is " + name + ", my age is " + age)
+print("I hope to work for " + dreamJob + " and earn $" + str(dreamSalary * 12) + " per year.")
+
+'''
+Please enter your name: Kenny Nguyen
+Please enter your age: 24
+Enter the company you wish to work for: Samsung
+Enter monthly salary you wish to earn in dollars: 9000
+
+My name is Kenny Nguyen, my age is 24
+I hope to work for Samsung and earn $108000 per year.
+'''
+
+'''
+Please enter your name: Michelle Pham
+Please enter your age: 33
+Enter the company you wish to work for: Valve
+Enter monthly salary you wish to earn in dollars: 10000
+
+My name is Michelle Pham, my age is 33
+I hope to work for Valve and earn $120000 per year.
+'''
